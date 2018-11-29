7ad02f88abe76e94c26bf4eeab884f67673bf767
pachas7 <josedavid.b77@gmail.com>
2018-11-29 10:06:59 +0100

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+def suma(a,b):
+  res = suma(3,2)
+  return "Hello World! %s" % (res)
+  
+if __name__== "__main__":
+  app.run(host='0.0.0.0', port=5000)
